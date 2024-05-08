5a33fbc5a5b87468621bddb836a5533a9817d062
wz <17600820823@163.com>
2024-05-08 16:23:29 +0800

    写接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, Query, Headers } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  constructor(private readonly userService: UserService) {
   }
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  findAll(@Query() req: any) {
+    console.log(req);
+    return {
+      statusCode: 200,
+      ...req
+    };
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @Post()
+  create(@Body('name') name: any, @Request() request) {
+    return {
+      statusCode: 200,
+      message: {
+        name
+      }
+    };
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Get(':id/:name')
+  findId(@Param("id") id, @Param("name") name, @Headers() { token }) {
+    console.log(id);
+    console.log(name);
+    console.log(token);
+    return {
+      statusCode: 200,
+      message: {
+        name, id, token
+      }
+    };
+
   }
 }
+
