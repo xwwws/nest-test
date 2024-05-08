3cfac3c5fc7cfd8bd07c97687c2de27be0725fb2
wz <17600820823@163.com>
2024-05-08 14:41:43 +0800

    生成curd模版

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestController } from './test/test.controller';
+import { TestModule } from './test/test.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TestModule, UserModule],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
