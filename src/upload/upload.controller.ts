fe5c2fd3c55f4b5c500f244fa00a8881d4648090
wz <17600820823@163.com>
2024-05-09 15:50:12 +0800

    feat: 下载文件

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -13,21 +13,42 @@ import {
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { FileInterceptor } from "@nestjs/platform-express";
+import { Response } from 'express';
+import { join } from "path";
+import { zip } from "compressing";
 
 @Controller('upload')
 export class UploadController {
-  constructor(private readonly uploadService: UploadService) {}
+  constructor(private readonly uploadService: UploadService) {
+  }
 
   @Post('album')
   @UseInterceptors(FileInterceptor('file'))
   upload(@UploadedFile() file, @Res() res) {
-    console.log(file);
     res.send({
       code: 200,
       data: {
         fileName: file.originalname
       }
-
-    })
+    });
   }
+
+  @Get('export')
+  download(@Res() res: Response) {
+    const imageUrl: string = join(__dirname, '../images/1715236711705.png');
+    res.download(imageUrl);
+  }
+
+
+  @Get('stream')
+  async downloadStream(@Res() res: Response) {
+    const imageUrl: string = join(__dirname, '../images/1715236711705.png');
+    const tarStream = new zip.Stream();
+    await tarStream.addEntry(imageUrl);
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', 'attachment; filename=wz');
+    tarStream.pipe(res)
+  }
+
+
 }
