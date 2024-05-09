159fd960272c6358f2b553e1bcea4bd5c80a820b
wz <17600820823@163.com>
2024-05-09 11:23:23 +0800

    feat: 依赖注入的中间件

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -6,6 +6,7 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log(req);
     console.log(`[Logger]-${req.path}`);
+    // res.send('拦截了')
     next();
   }
 }
