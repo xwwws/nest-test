b27a673130d7febb1800d383be9a3c01580effb2
wz <17600820823@163.com>
2024-05-11 10:21:45 +0800

    feat: 爬虫   写入文件

--- src/spider/spider.service.ts
+++ src/spider/spider.service.ts
@@ -3,26 +3,34 @@ import { CreateSpiderDto } from './dto/create-spider.dto';
 import { UpdateSpiderDto } from './dto/update-spider.dto';
 import axios from "axios";
 import * as cheerio from 'cheerio'
+import * as fs from "fs";
+import * as path from "path";
 @Injectable()
 export class SpiderService {
-  getSpider() {
+  async getSpider() {
     const urls: string[] = []
-    const baseUrl : string = 'https://bj.ke.com'
     const getPage = async () => {
       const url = `https://bj.ke.com/ershoufang/`
       console.log(`loading....`);
       const page = await axios.get(url).then(res => res.data)
-      console.log(page);
       const $ = cheerio.load(page)
-      $('.sellListContent li a img').each(function () {
-        urls.push(`${baseUrl}${$(this).attr('src')}`)
+      $('.sellListContent li a .lj-lazy').each(function () {
+        urls.push(`${$(this).attr('data-original')}`)
       })
-
-      console.log(urls);
     }
-
-    getPage()
+    await getPage()
+    await this.writeImages(urls)
+    console.log('写入完成');
     return '12'
+  }
+
+  async writeImages(urls: string[]) {
+    urls.forEach(async url => {
+      console.log(url);
+      const buffer = await axios.get(url,{responseType: 'arraybuffer'}).then(res => res.data)
+      const ws = fs.createWriteStream(path.join(__dirname, `../images/house_${new Date().getTime()}.jpg`))
+      ws.write(buffer)
+    })
   }
 
 }
